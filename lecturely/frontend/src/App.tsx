8e31cbd394e5c6941a24fe8844163719ba781abb
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2024-11-06 15:08:07 +0000

    Revert "Trying to make the landing page"
    This reverts commit 9080339268f7e55f36e342a052c526c961124295.

--- lecturely/frontend/src/App.tsx
+++ lecturely/frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import DashboardPage from './app/Lecturely/page';
+import React, { useEffect, useState } from 'react';
+
+interface Message {
+  message: string;
+}
 
 function App() {
-    return (
-        <DashboardPage />
-    );
+  const [message, setMessage] = useState<string>('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+        .then(response => response.json())
+        .then((data: Message) => setMessage(data.message))
+        .catch((error) => console.error("Error fetching message:", error));
+  }, []);
+
+  return (
+      <div>
+        <h1>React with TypeScript</h1>
+        <p>{message}</p>
+      </div>
+  );
 }
 
 export default App;
