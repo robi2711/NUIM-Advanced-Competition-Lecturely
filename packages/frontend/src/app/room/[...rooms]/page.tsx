cc4b49f6e83240b28f733f867be63ba607c1fe0b
Raoul <rhaba91@gmail.com>
2025-02-18 19:03:04 +0000

    Implemented Dynamic Routing + Webpages.
    - Display's Room Info
    - Dynamic Routing set up
    - Checks if Author or not

--- packages/frontend/src/app/room/[...rooms]/page.tsx
+++ packages/frontend/src/app/room/[...rooms]/page.tsx
@@ -1,30 +1,36 @@
 "use client"
 
+
 import * as React from 'react';
 import { CssBaseline } from '@mui/material';
 import { useParams } from 'next/navigation';
-import { getUser } from "@/app/components/services/UserService";
 import {useUser} from "@/app/components/services/UserContext";
 import api from "@/app/components/services/apiService";
+import NavBar from "@/app/components/common/MainNav";
 
 export default function Rooms() {
+    interface RoomInfo {
+        authorSub: string;
+    }
+
     const params = useParams();
     const { userInfo } = useUser();
+    const [roomInfo, setRoomInfo] = React.useState<RoomInfo | null > (null);
 
     React.useEffect(() => {
         const getRoomInfo = async () => {
             const rooms = params.rooms;
-            if(rooms){
+            if (rooms) {
                 try {
                     const response = await api.post('/db/getItem', {
                         TableName: 'TestTable',
                         itemAttributes: {
                             PK: rooms[0],
-                            SK:"room",
+                            SK: "room",
                         }
                     });
                     console.log(response.data);
-                    return response.data;
+                    setRoomInfo(response.data as any);
                 } catch (error) {
                     console.error(error);
                 }
@@ -32,27 +38,27 @@ export default function Rooms() {
         }
 
         getRoomInfo();
-    }, []);
+    }, [params]);
 
 
     return (
         <CssBaseline>
             <div>
                 <div>
-                    {userInfo ? (
+                    {userInfo && roomInfo ? (
                         <div>
                             <p>Username: {userInfo.username}</p>
                             <p>Email: {userInfo.email}</p>
                             <p>Sub: {userInfo.sub}</p>
                             <p>Rooms: {params.rooms}</p>
-                            <p>Author Sub: {params.authorSub}</p>
+                            <p>Author Sub: {roomInfo.authorSub}</p>
                         </div>
                     ) : (
                         <p>Loading user info...</p>
                     )}
 
-                    {userInfo && params && userInfo.sub !== params.sub ? (
-                        <p>Room ID: {userInfo.rooms[0]}</p>
+                    {userInfo && params && userInfo.sub === roomInfo?.authorSub ? (
+                        <NavBar />
                     ) : (
                         <p>No room information available</p>
                     )}
