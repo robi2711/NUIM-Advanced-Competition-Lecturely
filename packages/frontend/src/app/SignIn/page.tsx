a00bb9979248695d63183747e20cda06e337c65f
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2025-02-19 18:38:24 +0000

    a

--- packages/frontend/src/app/SignIn/page.tsx
+++ packages/frontend/src/app/SignIn/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 import * as React from "react";
 import SignIn from "@/app/components/authentication/SignIn";
+import {Suspense } from 'react';
 
 export default function SignInPage() {
 
 	return (
+		<Suspense fallback={<div>Loading...</div>}>
 		<SignIn/>
+		</Suspense>
 	);
 }
