e8ba07dac35329fdc80c681c1f5a0992510f7d85
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2025-02-22 14:07:38 +0000

    Refixed mic not closing 😭

--- packages/frontend/src/app/components/RoomManagement/LectureContent.tsx
+++ packages/frontend/src/app/components/RoomManagement/LectureContent.tsx
@@ -3,7 +3,7 @@
 import { Box, Paper, Typography, Button, List, ListItem, ListItemText, CssBaseline } from "@mui/material"
 import api from "@/app/components/services/apiService"
 import { useRouter } from "next/navigation"
-import {useEffect, useRef, useState} from "react"
+import {useEffect, useState} from "react"
 
 const users = ["Alice Smith", "Bob Johnson", "Charlie Brown", "Diana Prince", "Ethan Hunt"]
 
@@ -20,7 +20,7 @@ interface LectureViewProps {
 export default function LectureView({ roomInfo }: LectureViewProps) {
 	const router = useRouter()
 	const [transcript, setTranscript] = useState("")
-	const recognitionRef = useRef<SpeechRecognition | null>(null)
+
 
 	const sendPhrase = async (roomPK: string, phrase: string) => {
 		try {
@@ -53,7 +53,6 @@ export default function LectureView({ roomInfo }: LectureViewProps) {
 	const handleTranscription = (roomInfo: RoomInfo) => {
 		const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 		const recognition = new SpeechRecognition()
-		recognitionRef.current = recognition
 		let liveTranscript = ""
 
 		recognition.lang = "en-GB"
@@ -69,15 +68,21 @@ export default function LectureView({ roomInfo }: LectureViewProps) {
 
 		recognition.start()
 
+		if (window.location.pathname !== `/room/${roomInfo.PK}`) {
+			recognition.stop();
+			return;
+		}
+
 		recognition.onend = () => {
-			recognition.start()
+			if (window.location.pathname !== `/room/${roomInfo.PK}`) {
+				recognition.stop()
+			} else {
+				recognition.start()
+			}
 		}
 	}
 
 	const handleClose = () => {
-		if (recognitionRef.current) {
-			recognitionRef.current.stop()
-		}
 		router.push("/Lecturely")
 	}
 
