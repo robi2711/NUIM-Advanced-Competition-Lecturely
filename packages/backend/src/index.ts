355ebeefdd6cea5f170b081d4db8a61aecebc82c
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2025-04-16 14:58:28 +0100

    redeploy

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-	origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+	origin: "https://lecturely.vercel.app/",
 	credentials: true
 }));
 
--- packages/frontend/src/app/components/services/apiService.ts
+++ packages/frontend/src/app/components/services/apiService.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_BACKEND_URL ,
+	baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001",
+	withCredentials: true,
 	headers: {
 		'Content-Type': 'application/json',
-
 	}
 });
 
